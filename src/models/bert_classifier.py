361ef009da81ca77733c2095a41e9241dab911d2
dan-asu <dhlevca@asu.edu>
2026-03-03 16:29:34 -0700

    clarification added to bert_classifier and to train.py

--- src/models/bert_classifier.py
+++ src/models/bert_classifier.py
@@ -1,7 +1,10 @@
 
 from transformers import AutoConfig, AutoModelForSequenceClassification
 
-
+# the model will need as input 3 things:
+# 1) pretrained_name, a string: name of the model (eg. "bert-base-uncased")
+# 2) num_labels, integer with the number of classes to predict (probably 2 if we wnat to detect good software and bad software)
+# 3) dropout, float with the regularization strength (eg. 0.1)
 def build_model(pretrained_name: str, num_labels: int = 2, dropout: float = 0.1):
     cfg = AutoConfig.from_pretrained(pretrained_name, num_labels=num_labels)
     # this makes our model more robust, it controls the dropout in the hidden layers / outputs, regularizes dropouts..we need to read more about this
--- src/train.py
+++ src/train.py
@@ -1,1 +1,27 @@
+# getting the model and make it ready for the training
 
+from src.models.bert_classifier import build_model
+
+# waht we can use for good/bad software detection/quality classification
+# 1) "microsoft/codebert-base" (Best for:Defects4J,Bugs.jar,Function-level classification)
+# 2) "microsoft/codebert-base-mlm" (best if we want to pretrain before fine tunning)
+# 3) "microsoft/graphcodebert-base" (Best for:Bug detection, Code vulnerability classification,When semantic structure matters) 
+# 4) "huggingface/CodeBERTa-small-v1" ( Best for:Fast experiments, Limited GPU, Quick iteration)
+### I guess the best option would be the 4th one + freezing the transformer since we have no GPU to fine tune. with shorter sequence length can be done
+### Our choice is dictated by the computational power mostly...correct me if I am wrong
+
+MODEL_NAME = "huggingface/CodeBERTa-small-v1"
+
+# how many classes we have
+NUM_LABELS = 2
+
+# Dataset is small → dropout increase (0.2–0.3)
+# Dataset is large → dropout default 0.1 
+DROPOUT = 0.1
+
+
+model = build_model(
+    pretrained_name=MODEL_NAME,
+    num_labels=NUM_LABELS,
+    dropout=DROPOUT
+)
